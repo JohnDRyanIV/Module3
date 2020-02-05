9905df064a4cb4e6eacb0b755d027c171d09d3d1
Diamondfiend <jdryan30@yahoo.com>
2020-02-05 16:23:23 -0600

    added average implementation, test ran correctly

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -1,2 +1,9 @@
 def average():
-    pass
+    # Get input for scores and determine average
+
+    score1 = input("Enter first grade: ")
+    score2 = input("Enter second grade: ")
+    score3 = input("Enter third grade: ")
+    return (score1 + score2 + score3) / 3
+
+
